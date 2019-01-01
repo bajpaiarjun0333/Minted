ac33de18ccb8e32068201bfd1d285f9a1941ec77
Arjun Bajpai <bajpaiarjun0333@gmail.com>
2019-01-01 11:42:43 +0530

    Initial commit

---
+++ app/src/main/java/com/bajpaiarjun0333/minted/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bajpaiarjun0333.minted;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
